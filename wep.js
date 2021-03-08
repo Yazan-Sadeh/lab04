91233d90a82a36c15ba4647321f827ab60e0c520
Yazan Sadeh <79832871+Yazan-Sadeh@users.noreply.github.com>
2021-03-08 09:07:30 +0200

    Add files via upload

---
+++ wep.js
@@ -0,0 +1,22 @@
+console.log("jordan");
+console.log(2030);
+console.log("Yazan Sadeh");
+alert ("good morning")
+var age = prompt("Enter your age");
+if (age>32){
+    alert("Yes correcta");
+}
+else if (age<20){
+    alert("you are wrong");
+}
+
+
+var S = 100
+var X = 70
+console.log (S<X)
+console.log (S>X)
+console.log (S==X)
+
+
+document.write("I'm good");
+document.write("<h1>I'm good</h1><p>I hope you are well!</p>");
